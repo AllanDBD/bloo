c3f5c5a658df9583fab88bd19eee87ae5a701e70
Norman ALIE <mail@normanalie.fr>
2023-03-20 20:35:11 +0100

    Rect primary

--- frontend/src/components/buttons/RectPrimary.vue
+++ frontend/src/components/buttons/RectPrimary.vue
@@ -19,15 +19,11 @@
 
 <template>
     <button
-        class="flex h-[36px] w-[256px] flex-row items-center gap-2 rounded-[8px] p-3 text-content-primaryDark disabled:text-content-disabled bg-foundation-primaryA disabled:bg-background-disabled hover:bg-content-accent"
+        class="text-sm font-medium flex h-[36px] w-[256px] flex-row items-center gap-2 rounded-[8px] p-3 text-content-primaryDark disabled:text-content-disabled bg-foundation-primaryA disabled:bg-background-disabled hover:bg-content-accent"
     >
-        <div
-            class="flex h-[16px] w-[232px] flex-row items-center justify-center gap-2 p-0"
-        >
+        <div class="flex w-full flex-row items-center justify-center gap-2 p-0">
             <slot name="icon"></slot>
-            <span
-                class="items-center text-center flex h-[16px] w-[37px] text-sm font-medium"
-            >
+            <span class="items-center text-center flex">
                 <slot></slot>
             </span>
         </div>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,14 +1,25 @@
 <script>
+import RectPrimary from '../components/buttons/RectPrimary.vue';
 import TheHelloWorld from '../components/TheHelloWorld.vue';
+import rocket_launch from '../components/icons/rocket_launch.vue';
+
 export default {
     components: {
         TheHelloWorld,
+        RectPrimary,
+        rocket_launch,
     },
 };
 </script>
 
 <template>
-    <main>
+    <main class="flex flex-col">
         <TheHelloWorld />
+        <RectPrimary class="w-[512px] h-[46px] text-base m-auto mt-10">
+            <template v-slot:icon>
+                <rocket_launch fill="currentColor" width="20px" height="20px" />
+            </template>
+            Let's Bloo!
+        </RectPrimary>
     </main>
 </template>
