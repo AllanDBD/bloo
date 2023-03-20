7fe1929762ab3dff04e723e93a1d08c57a2a383e
Norman ALIE <mail@normanalie.fr>
2023-03-20 23:20:04 +0100

    Rect secondary

---
+++ frontend/src/components/buttons/RectSecondary.vue
@@ -0,0 +1,31 @@
+<!--
+# Usage (no icon):
+<RectSecondary>
+    Label
+</RectSecondary>
+
+# Usage (with icon):
+<RectSecondary>
+    <template v-slot:icon>
+        <rocket_launch fill="currentColor" width="16px" height="16px" />
+    </template>
+    Label
+</RectSecondary>
+
+
+-->
+
+<script></script>
+
+<template>
+    <button
+        class="text-sm font-medium flex h-[36px] w-[256px] flex-row items-center gap-2 rounded-[8px] p-3 text-foundation-primaryA disabled:text-content-disabled border-border-selectedLight disabled:border-border-disabled border-2 hover:border-border-accent bg-transparent hover:text-content-accent"
+    >
+        <div class="flex w-full flex-row items-center justify-center gap-2 p-0">
+            <slot name="icon"></slot>
+            <span class="items-center text-center flex">
+                <slot></slot>
+            </span>
+        </div>
+    </button>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,17 @@
 <script>
-import RectPrimary from '../components/buttons/RectPrimary.vue';
 import TheHelloWorld from '../components/TheHelloWorld.vue';
+import RectPrimary from '../components/buttons/RectPrimary.vue';
+import RectSecondary from '../components/buttons/RectSecondary.vue';
 import rocket_launch from '../components/icons/rocket_launch.vue';
+import bed from '../components/icons/bed.vue';
 
 export default {
     components: {
         TheHelloWorld,
         RectPrimary,
+        RectSecondary,
         rocket_launch,
+        bed,
     },
 };
 </script>
@@ -15,11 +19,20 @@ export default {
 <template>
     <main class="flex flex-col">
         <TheHelloWorld />
-        <RectPrimary class="w-[512px] h-[46px] text-base m-auto mt-10">
+        <RectPrimary class="md:w-[512px] h-[46px] text-base m-auto mt-10">
             <template v-slot:icon>
                 <rocket_launch fill="currentColor" width="20px" height="20px" />
             </template>
             Let's Bloo!
         </RectPrimary>
+        <RectSecondary
+            class="md:w-[512px] h-[46px] text-base m-auto mt-10"
+            disabled
+        >
+            <template v-slot:icon>
+                <bed fill="currentColor" width="20px" height="20px" />
+            </template>
+            I don't care
+        </RectSecondary>
     </main>
 </template>
