28e6a06e7c4aed73ffc7e7a1fa013795ba06de56
Norman ALIE <mail@normanalie.fr>
2023-03-21 00:31:00 +0100

    Create SquareSecondary.vue

---
+++ frontend/src/components/buttons/SquareSecondary.vue
@@ -0,0 +1,18 @@
+<script>
+import rocket_launch from '../icons/rocket_launch.vue';
+export default {
+    components: {
+        rocket_launch,
+    },
+};
+</script>
+
+<template>
+    <button
+        class="flex h-9 w-9 flex-row items-center justify-center gap-2 rounded-lg border-2 border-content-secondaryLight bg-transparent text-content-secondaryLight hover:border-content-selected hover:text-content-selected disabled:border-border-disabled disabled:text-content-disabled dark:border-content-secondaryDark dark:text-content-secondaryDark dark:hover:border-content-selected dark:hover:text-content-selected"
+    >
+        <slot>
+            <rocket_launch fill="currentColor" class="h-4 w-4" />
+        </slot>
+    </button>
+</template>
