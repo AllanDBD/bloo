fa50fb0fe5322e9508147f8c9b19ea5b71865bff
Norman ALIE <mail@normanalie.fr>
2023-03-20 23:55:18 +0100

    Create PillSecondary.vue

---
+++ frontend/src/components/buttons/PillSecondary.vue
@@ -0,0 +1,11 @@
+<template>
+    <button
+        class="w-[61px] h-[36px] flex flex-col justify-center items-center bg-transparent hover:bg-content-accent hover:text-content-primaryDark border-2 border-border-accent rounded-full font-medium text-content-accent text-sm disabled:bg-border-disabled disabled:border-border-disabled disabled:text-border-opaqueLight"
+    >
+        <div
+            class="flex flex-row justify-center items-center w-full h-full pt-1"
+        >
+            <slot class="flex items-center text-center">Label</slot>
+        </div>
+    </button>
+</template>
