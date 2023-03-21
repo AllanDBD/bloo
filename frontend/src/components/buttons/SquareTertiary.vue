a9092ce8037097fa23e84af591c08c0b378aca71
Norman ALIE <mail@normanalie.fr>
2023-03-21 15:41:44 +0100

    Create SquareTertiary.vue

---
+++ frontend/src/components/buttons/SquareTertiary.vue
@@ -0,0 +1,9 @@
+<script></script>
+
+<template>
+    <button
+        class="flex h-9 w-9 flex-row items-center justify-center gap-2 rounded-lg border-2 border-content-tertiaryLight bg-transparent pt-1 text-sm font-medium text-content-tertiaryLight hover:border-border-opaqueLight disabled:border-transparent disabled:text-content-disabled dark:border-content-tertiaryDark dark:text-content-tertiaryDark dark:hover:border-border-opaqueDark dark:disabled:border-border-opaqueDark dark:disabled:text-border-opaqueDark"
+    >
+        <slot> 01 </slot>
+    </button>
+</template>
