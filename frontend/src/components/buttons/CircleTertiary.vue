496a0232cf3f854b1789d7a42ad4fb5d314e1da6
Norman ALIE <mail@normanalie.fr>
2023-03-21 16:31:37 +0100

    Create Circle Tertiary

--- frontend/src/components/buttons/CircleTertiary.vue
+++ frontend/src/components/buttons/CircleTertiary.vue
@@ -1,19 +1,7 @@
-<!--
-# Usage (text):
-<CircleTertiary>
-    Label
-</CircleTertiary>
-
-# Usage (icon):
-<CircleTertiary>
-    <rocket_launch fill="currentColor" width="16px" height="16px"/>
-</CircleTertiary>
--->
-
 <template>
     <button
-        class="hover:text-content-selecte flex h-9 w-9 flex-row items-center justify-center gap-[10px] rounded-full border-2 border-content-tertiaryLight text-sm font-medium leading-[14px] text-content-tertiaryLight hover:border-border-opaqueLight  disabled:border-border-disabled disabled:text-content-disabled dark:border-content-tertiaryDark dark:text-content-tertiaryDark dark:hover:border-content-selected dark:hover:text-content-selected dark:disabled:border-border-disabled dark:disabled:text-content-disabled"
+        class="flex h-9 w-9 flex-row items-center justify-center gap-[10px] rounded-full border-2 border-content-tertiaryLight p-[10px] text-sm font-medium text-content-tertiaryLight hover:border-border-opaqueLight hover:text-border-opaqueLight disabled:border-background-disabled disabled:text-content-disabled dark:border-content-tertiaryDark dark:text-content-tertiaryDark dark:hover:border-border-opaqueDark hover:dark:text-border-opaqueDark dark:disabled:border-background-disabled dark:disabled:text-content-disabled"
     >
-        <slot> 01 </slot>
+        <slot>01</slot>
     </button>
 </template>
