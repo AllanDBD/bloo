f9edcade22515f8dc8993d4d05b7960cec1f8986
Norman ALIE <mail@normanalie.fr>
2023-03-21 15:32:37 +0100

    Add text label

--- frontend/src/components/buttons/SquareSecondary.vue
+++ frontend/src/components/buttons/SquareSecondary.vue
@@ -1,18 +1,9 @@
-<script>
-import rocket_launch from '../icons/rocket_launch.vue';
-export default {
-    components: {
-        rocket_launch,
-    },
-};
-</script>
+<script></script>
 
 <template>
     <button
-        class="flex h-9 w-9 flex-row items-center justify-center gap-2 rounded-lg border-2 border-content-secondaryLight bg-transparent text-content-secondaryLight hover:border-content-selected hover:text-content-selected disabled:border-border-disabled disabled:text-content-disabled dark:border-content-secondaryDark dark:text-content-secondaryDark dark:hover:border-content-selected dark:hover:text-content-selected dark:disabled:border-border-disabled dark:disabled:text-content-disabled"
+        class="flex h-9 w-9 flex-row items-center justify-center gap-2 rounded-lg border-2 border-content-secondaryLight bg-transparent text-sm font-medium text-content-secondaryLight hover:border-content-selected hover:text-content-selected disabled:border-border-disabled disabled:text-content-disabled dark:border-content-secondaryDark dark:text-content-secondaryDark dark:hover:border-content-selected dark:hover:text-content-selected dark:disabled:border-border-disabled dark:disabled:text-content-disabled"
     >
-        <slot>
-            <rocket_launch fill="currentColor" class="h-4 w-4" />
-        </slot>
+        <slot> 01 </slot>
     </button>
 </template>
