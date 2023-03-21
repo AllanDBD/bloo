0cda6f30011b2635f4fb72b25ee2f0d1e603d5ae
Norman ALIE <mail@normanalie.fr>
2023-03-21 09:08:07 +0100

    Disabled dark state

--- frontend/src/components/buttons/SquareSecondary.vue
+++ frontend/src/components/buttons/SquareSecondary.vue
@@ -9,7 +9,7 @@ export default {
 
 <template>
     <button
-        class="flex h-9 w-9 flex-row items-center justify-center gap-2 rounded-lg border-2 border-content-secondaryLight bg-transparent text-content-secondaryLight hover:border-content-selected hover:text-content-selected disabled:border-border-disabled disabled:text-content-disabled dark:border-content-secondaryDark dark:text-content-secondaryDark dark:hover:border-content-selected dark:hover:text-content-selected"
+        class="flex h-9 w-9 flex-row items-center justify-center gap-2 rounded-lg border-2 border-content-secondaryLight bg-transparent text-content-secondaryLight hover:border-content-selected hover:text-content-selected disabled:border-border-disabled disabled:text-content-disabled dark:border-content-secondaryDark dark:text-content-secondaryDark dark:hover:border-content-selected dark:hover:text-content-selected dark:disabled:border-border-disabled dark:disabled:text-content-disabled"
     >
         <slot>
             <rocket_launch fill="currentColor" class="h-4 w-4" />
